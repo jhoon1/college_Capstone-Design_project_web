1a4c2a231cb8ca06e0eb7456e1dccb05ef9055f6
jhoon1 <103561706+jhoon1@users.noreply.github.com>
2022-05-18 15:08:25 +0900

    주문 리스트 완성 추후 firebase와 로봇 연동 및 웹 페이지 완성 예정

--- item.js
+++ item.js
@@ -42,61 +42,95 @@ function listDelete(all){
 }
 
 function itemGolist(all, detail, at, number, name, self2, no, obn){
-    var ml = document.createElement('div');
-    ml.id = all;
-    document.getElementById('order_list').appendChild(ml);
-    var il = document.createElement('div');
-    il.id = detail;
-    il.className = 'item_list';
-    document.getElementById(all).appendChild(il);
-    var lmn = document.createElement('div');
-    lmn.id = at;
-    document.getElementById(detail).appendChild(lmn);
-    getText(name, at)
-    var i_n = document.createElement('div');
-    i_n.id = number;
-    i_n.className = 'item_number';
-    document.getElementById(detail).appendChild(i_n);
-    var inm = document.createElement('input');
-    inm.type = 'button';
-    inm.className = 'item_number_minus';
-    inm.value = '-';
-    // document.getElementById(number).firstElementChild.addEventListener('click', function(){
-    //     buttonchangeValue(self, self2);
-    // });
-    document.getElementById(number).appendChild(inm);
-    var inv = document.createElement('input');
-    inv.type = 'text';
-    inv.id = self2;
-    inv.className = 'item_number_value';
-    inv.value = document.getElementById(obn).value;
-    inv.maxlength = '6';
-    inv.autocomplete = 'off';
-    // inv.addEventListener('input', function(){
-    //     textValue(self);
-    // });
-    document.getElementById(number).appendChild(inv);
-    var inp = document.createElement('input');
-    inp.type = 'button';
-    inp.className = 'item_number_plus';
-    inp.value = '+';
-    // inp.addEventListener('click', function(){
-    //     buttonchangeValue(self, self2);
-    // });
-    document.getElementById(number).appendChild(inp);
-    var c = document.createElement('div');
-    c.id = no;
-    c.className = 'cancel';
-    document.getElementById(detail).appendChild(c);
-    var cb = document.createElement('input');
-    cb.type = 'button';
-    cb.className = 'cancel_button';
-    cb.value = 'X';
-    cb.addEventListener('click', function(){
-        listDelete(all);
-    });
-    document.getElementById(no).appendChild(cb);
-    // listDelete 함수 다시보고 만들면 click 함수들이 안들어감 그거 살펴보기 + 담기 value사라짐
+    if(document.getElementById(all)){
+        
+    }
+    else{
+        var ml = document.createElement('div');
+        ml.id = all;
+        document.getElementById('order_list').appendChild(ml);
+        var il = document.createElement('div');
+        il.id = detail;
+        il.className = 'item_list';
+        document.getElementById(all).appendChild(il);
+        var lmn = document.createElement('div');
+        lmn.id = at;
+        document.getElementById(detail).appendChild(lmn);
+        getText(name, at)
+        var i_n = document.createElement('div');
+        i_n.id = number;
+        i_n.className = 'item_number';
+        document.getElementById(detail).appendChild(i_n);
+        var inm = document.createElement('input');
+        inm.type = 'button';
+        inm.className = 'item_number_minus';
+        inm.value = '-';
+        // document.getElementById(number).firstElementChild.addEventListener('click', function(){
+        //     buttonchangeValue(self, self2);
+        // });
+        document.getElementById(number).appendChild(inm);
+        var inv = document.createElement('input');
+        inv.type = 'text';
+        inv.id = self2;
+        inv.className = 'item_number_value';
+        inv.value = document.getElementById(obn).value;
+        inv.maxlength = '6';
+        inv.autocomplete = 'off';
+        // inv.addEventListener('input', function(){
+        //     textValue(self);
+        // });
+        document.getElementById(number).appendChild(inv);
+        var inp = document.createElement('input');
+        inp.type = 'button';
+        inp.className = 'item_number_plus';
+        inp.value = '+';
+        // inp.addEventListener('click', function(){
+        //     buttonchangeValue(self, self2);
+        // });
+        document.getElementById(number).appendChild(inp);
+        var c = document.createElement('div');
+        c.id = no;
+        c.className = 'cancel';
+        document.getElementById(detail).appendChild(c);
+        var cb = document.createElement('input');
+        cb.type = 'button';
+        cb.className = 'cancel_button';
+        cb.value = 'X';
+        cb.addEventListener('click', function(){
+            listDelete(all);
+        });
+        document.getElementById(no).appendChild(cb);
+        var btm = document.getElementById(number).firstElementChild;
+        btm.addEventListener('click', function(){
+            buttonchangeValue(this, self2);
+        });
+        var btp = document.getElementById(number).lastElementChild;
+        btp.addEventListener('click', function(){
+            buttonchangeValue(this, self2);
+        });
+        var btv = document.getElementById(self2);
+        btv.addEventListener('input', function(){
+            textValue2(self2);
+        });
+    }
+}
+
+function itemGolist2(all, detail, at, number, name, self2, no, obn){
+    var ht = `<div id=${all}>
+    <div id=${detail} class="item_list">
+        <div id=${at}>
+        </div>
+        <div id=${number} class="item_number">
+            <input type="button" class="item_number_minus" value="-">
+            <input type="text" id=${self2} class="item_number_value" value=${document.getElementById(obn).value} maxlength="6" autocomplete="off">
+            <input type="button" class="item_number_plus" value="+">
+        </div>
+        <div id=${no} class="cancel">
+            <input type="button" class="cancel_button" value="X">
+        </div>
+    </div>
+    </div>`;
+    document.getElementById('order_list').append(ht);
     var btm = document.getElementById(number).firstElementChild;
     btm.addEventListener('click', function(){
         buttonchangeValue(this, self2);
@@ -108,5 +142,10 @@ function itemGolist(all, detail, at, number, name, self2, no, obn){
     var btv = document.getElementById(self2);
     btv.addEventListener('input', function(){
         textValue2(self2);
+    });
+    getText(name ,at);
+    var cb = document.getElementById(no).firstElementChild;
+    cb.addEventListener('click', function(){
+        listDelete(all);
     });
 }
